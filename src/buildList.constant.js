936086a607f02ec6fa6db2d00720a27d69dad2e9
Rahul Shah <rahul.shah9863@gmail.com>
2025-01-25 22:51:02 +0545

    feat: object for easy access for constants

--- src/buildList.constant.js
+++ src/buildList.constant.js
@@ -16,16 +16,6 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://ipfs.io/ipfs/bafkreicy5wj4ahfeaopdfpduna3yf7klfl6nkum6pg4buwj6cdjmvegilu",
     keywords: ["default", "storyhunt", "constants", "list"],
-    contants: [...core_contracts, ...periphery_contracts, ...graph]
-      // sort them by symbol for easy readability
-      .sort((t1, t2) => {
-        if(!t1.address || !t2.address) {
-          return 0;
-        }
-        if (t1.chainId === t2.chainId) {
-          return t1.address?.toLowerCase() < t2.address?.toLowerCase() ? -1 : 1;
-        }
-        return t1.chainId < t2.chainId ? -1 : 1;
-      }),
+    constants: {...core_contracts, ...periphery_contracts, ...graph}
   };
 };
