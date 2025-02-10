7b382e39e9adb326a28f30018d1173d89d7db045
Rahul Shah <rahul.shah9863@gmail.com>
2025-02-10 15:34:44 +0545

    update: addresses

--- src/buildList.constant.js
+++ src/buildList.constant.js
@@ -1,12 +1,26 @@
 const { version } = require("../package.json");
-const core_contracts = require("./constants/core-contract.json");
-const periphery_contracts = require("./constants/periphery-contract.json");
-const constants = require("./constants/constants.json");
+
+//story
+const story_core_contracts = require("./constants/story/core-contract.json");
+const story_periphery_contracts = require("./constants/story/periphery-contract.json");
+const story_constants = require("./constants/story/constants.json");
+
+//aeneid
+const aeneid_core_contracts = require("./constants/aeneid/core-contract.json");
+const aeneid_periphery_contracts = require("./constants/aeneid/periphery-contract.json");
+const aeneid_constants = require("./constants/aeneid/constants.json");
+
+//odyssey
+const odyssey_core_contracts = require("./constants/odyssey/core-contract.json");
+const odyssey_periphery_contracts = require("./constants/odyssey/periphery-contract.json");
+const odyssey_constants = require("./constants/odyssey/constants.json");
 
 const ChainId = {
   ODYSSEY: 1516,
-  STORY: 1514
-}
+  STORY: 1514,
+  AENEID: 1315,
+};
+
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -19,9 +33,14 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "https://ipfs.io/ipfs/bafkreicy5wj4ahfeaopdfpduna3yf7klfl6nkum6pg4buwj6cdjmvegilu",
+    logoURI:
+      "https://ipfs.io/ipfs/bafkreicy5wj4ahfeaopdfpduna3yf7klfl6nkum6pg4buwj6cdjmvegilu",
     keywords: ["default", "storyhunt", "constants", "list"],
     chains: Object.values(ChainId),
-    constants: { [ChainId.ODYSSEY]: { ...core_contracts[ChainId.ODYSSEY], ...periphery_contracts[ChainId.ODYSSEY], ...constants[ChainId.ODYSSEY] }, [ChainId.STORY]: { ...core_contracts[ChainId.STORY], ...periphery_contracts[ChainId.STORY], ...constants[ChainId.STORY] } },
+    constants: {
+      [ChainId.STORY]: {...story_constants, ...story_core_contracts, ...story_periphery_contracts},
+      [ChainId.AENEID]: {...aeneid_constants, ...aeneid_core_contracts, ...aeneid_periphery_contracts},
+      [ChainId.ODYSSEY]: {...odyssey_constants, ...odyssey_core_contracts, ...odyssey_periphery_contracts},
+    },
   };
 };
