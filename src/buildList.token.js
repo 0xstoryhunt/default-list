2d583bfffdcad20d605d028740020664f6e15e1f
thegaslessninja <thegaslessninja@gmail.com>
2025-06-25 22:55:20 +0545

    add: ip.world tokens

--- src/buildList.token.js
+++ src/buildList.token.js
@@ -20,7 +20,7 @@ module.exports = function buildList() {
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
-          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+          return t1.address.toLowerCase() < t2.address.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
       }),
